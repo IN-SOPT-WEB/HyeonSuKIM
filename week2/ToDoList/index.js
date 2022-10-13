f47fc8a2793187dd24015409132fa00265f60fa3
borimong <kimhyunsu11@naver.com>
2022-10-14 04:42:41 +0900

    feat: todo리스트  add 기능 구현

--- week2/ToDoList/index.js
+++ week2/ToDoList/index.js
@@ -2,11 +2,19 @@ const $ = (selector) => document.querySelector(selector);
 const $$ = (selector) => document.querySelectorAll(selector);
 
 const nav = $('nav');
+const forms = $$('form');
+const left_form_button = $('.leftButton');
+const right_form_button = $('.rightButton');
+const left_input = $('.leftInput');
+const right_input = $('.rightInput');
+const left_list = $('.leftList');
+const right_list = $('.rightList');
 
 //이벤트 위임을 이용해 버튼을 이용한 화면 전환 구현
 //애니메이션 효과 적용
 
 nav.addEventListener("click", (e) => {
+    e.stopPropagation();
     const target = e.target;
     switch (target.id) {
         case 'today_button' :
@@ -30,4 +38,52 @@ nav.addEventListener("click", (e) => {
     }
     
 })
+
+//Left + 버튼 클릭 시 Add 함수 호출하는 이벤트 
+left_form_button.addEventListener("click", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    left_list.appendChild(onAddLeft(left_input));
+    
+})
+
+//Left 에 Add 하는 함수 
+
+function onAddLeft(left_input) {
+    const currentLi = document.createElement('li');
+    currentLi.innerHTML = left_input.value; 
+
+    //delete 아이콘 만들어서 리스트에 추가하기 
+    const currentDeleteBtn = document.createElement('span');
+    currentDeleteBtn.innerHTML = "delete";
+    currentDeleteBtn.classList.add('material-symbols-outlined');
+    currentLi.appendChild(currentDeleteBtn);
+    currentLi.classList.add('grid');
+
+    return currentLi;
+}
+
+//Right + 버튼 클릭 시 Add 함수 호출하는 이벤트 
+
+right_form_button.addEventListener("click", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    right_list.appendChild(onAddRight(right_input));
+})
+
+//Right 에 Add 하는 함수 
+
+function onAddRight(right_input) {
+    const currentLi = document.createElement('li');
+    currentLi.innerHTML = right_input.value; 
+
+    //delete 아이콘 만들어서 리스트에 추가하기 
+    const currentDeleteBtn = document.createElement('span');
+    currentDeleteBtn.innerHTML = "delete";
+    currentDeleteBtn.classList.add('material-symbols-outlined');
+    currentLi.appendChild(currentDeleteBtn);
+    currentLi.classList.add('grid');
+
+    return currentLi;
+}
 
