f7468dea686a3feee4ca71aa1c18bebe2399d142
borimong <kimhyunsu11@naver.com>
2022-10-14 15:39:53 +0900

    feat: velog 태그 추가 및 삭제 기능 구현

--- week2/Velog/index.js
+++ week2/Velog/index.js
@@ -5,6 +5,8 @@ const dropdown_toggle = $('.dropdownToggle');
 const dropdown_content = $('.dropdown_content');
 const toggle_content = $('.toggle_content');
 
+//dropdown 구현하기
+
 dropdown_toggle.addEventListener("click", (e) => {
     e.stopPropagation();
     dropdown_content.classList.add('show');
@@ -13,8 +15,6 @@ dropdown_toggle.addEventListener("click", (e) => {
 dropdown_content.addEventListener("click", (e) => {
     e.stopPropagation();
     dropdown_content.classList.remove('show');
-    console.log(e.target);
     toggle_content.innerHTML = e.target.innerText;
 })
 
-//dropdown 구현하기
